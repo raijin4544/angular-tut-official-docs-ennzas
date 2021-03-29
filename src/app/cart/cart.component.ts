fc08752cc9ae51208b7c2faafd4615c00d354e5e
raijin4544 <yashwanthdataentryworks@gmail.com>
2021-03-29 17:14:23 +0530

    4 files modified, 3 added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { CartService } from "../cart.service";
+import { FormBuilder } from "@angular/forms";
 @Component({
   selector: "app-cart",
   templateUrl: "./cart.component.html",
   styleUrls: ["./cart.component.css"]
 })
-export class CartComponent implements OnInit {
-  constructor(private cartService: CartService) {}
+export class CartComponent {
+  constructor(
+    private cartService: CartService,
+    private formBulider: FormBuilder
+  ) {}
   items = this.cartService.getItems();
-  ngOnInit() {}
+  checkoutForm = this.formBulider.group({
+    name: "",
+    address: ""
+  });
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn("Your order has been submitted", this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
